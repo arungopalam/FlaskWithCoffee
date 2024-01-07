8d05fd63b20210e3ab919c4bcb6954dbcf2a8657
arungopalam@gmail.com <Orangemilk1>
2024-01-07 18:32:53 +0000

    added pg

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,10 @@
 import os
 
+from click import echo
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import current_user, LoginManager, login_required, login_user, logout_user
+from flask_login import LoginManager
+import sqlalchemy as sa
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -21,8 +23,16 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
     from .models import User
-    with app.app_context():
-        db.create_all()
+    register_cli_commands(app)
+    engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+    inspector = sa.inspect(engine)
+    if not inspector.has_table("users"):
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
+            app.logger.info('Initialized the database!')
+    else:
+        app.logger.info('Database already contains the users table.')
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -32,3 +42,12 @@ def create_app():
         return User.query.get(int(user_id))
 
     return app
+
+
+def register_cli_commands(app):
+    @app.cli.command('init_db')
+    def initialize_database():
+        """Initialize the database."""
+        db.drop_all()
+        db.create_all()
+        echo('Initialized the database!')
